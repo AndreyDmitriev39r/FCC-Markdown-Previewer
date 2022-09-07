f5ef946dd3c9b3971007a35b68c68198c68c4245
Andrey Dmitriev <admit8490@gmail.com>
2022-09-07 13:23:22 +0200

    necessary components

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
-// constants
-
 const rootElement = document.getElementById('root');
+
+const Editor = () => {
+  return (
+    <textarea id="editor" />
+  )
+}
+
+const Preview = () => {
+  return (
+    <div id="preview" />
+  )
+}
+
+const App = () => {
+  return (
+    <>
+    <Editor />
+    <Preview />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, rootElement);
