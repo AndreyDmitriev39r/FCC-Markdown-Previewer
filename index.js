ae84e285623698b1c1ef268c4430f5c35452d5b9
Andrey Dmitriev <admit8490@gmail.com>
2022-09-16 00:20:03 +0200

    all tests passed except optional

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const rootElement = document.getElementById('root');
 
 const initialMarkdown = "# This is main heading\n## This is sub-heading\n[This is a link](URL)\n`this is inline code`\n```\nthis is code block\n```\n> this is blockquote\n\n![here goes sr-text](here/goes/the/path 'here goes text for mouseOver')\n**and, finally, this is fat and bolded text**\n* this is list item";
 
-
 // child components
 
 const Header = () => {
@@ -30,7 +29,7 @@ const Editor = ({defaultValue, changeHandler}) => {
 const Preview = ({text}) => {
   return (
     <div className='row justify-content-center mb-1'>
-      <div id="preview" className='col-10' children={text}/>
+      <div id="preview" className='col-10' dangerouslySetInnerHTML={{__html: text}}/>
     </div>
   )
 }
@@ -50,7 +49,7 @@ const Footer = () => {
 
 // main component
 
-const App = () => {
+const App = () => {  
   const [previewContent, setPreviewContent] = React.useState(marked.parse(initialMarkdown));
   const handleChange = (event) => {    
     setPreviewContent(marked.parse(event.target.value));
